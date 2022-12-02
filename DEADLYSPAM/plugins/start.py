2feb83f3f48734bdb6f2ae7802bd3eb457c121ae
Kamlesh chaurasiya <kamleshchaurasiya8052@gmail.com>
2022-12-02 19:28:30 +0530

    first commit

---
+++ DEADLYSPAM/plugins/start.py
@@ -0,0 +1,52 @@
+
+import asyncio
+import os
+from telethon import events, Button
+from telethon.tl.custom import button
+from DEADLYSPAM import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, ALIVE_PIC, OWNER_ID, OWNER_NAME
+
+DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/4f0335152fd7520a90799.jpg"
+
+
+Deadly_Button = [
+        [
+        Button.url("Cʜᴀɴɴᴇʟ", "https://t.me/KingHacker_spambot"),
+        Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/king_hacker_spam_bot")
+        ],
+        [
+        Button.url("• Rᴇᴘᴏ •", "https://github.com/Team-Deadly/DEADLY-SPAMBOT")
+        ]
+        ]
+        
+
+#USERS 
+
+
+@BOT0.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT1.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT2.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT3.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT4.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT5.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT6.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT7.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT8.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT9.on(events.NewMessage(incoming=True, pattern='/start'))
+async def start(e):
+    if e.chat_id is e.sender_id:
+        name = e.sender.first_name
+        user_id = e.sender_id
+        mention = f"[{name}](tg://user?id={user_id})"
+        myOwner = f"[{OWNER_NAME}](tg://user?id={OWNER_ID})"
+        creator = f"[KingHacker](tg://user?id={5583550015})"
+        DEADLY_ON = f"""
+ʜᴇʏ {mention},
+THIS IS KING HACKER SPAM BOT POWERED BY:- {creator}!
+
+ᴛʜɪs ʙᴏᴛ ᴏᴡɴᴇʀ:- {myOwner}
+
+ᴄᴏᴅᴇ ᴄʀᴇᴀᴛᴏʀ:- {creator}
+
+ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴀᴄᴄᴇss sᴜᴘᴘᴏʀᴛ ,ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ʀᴇᴘᴏ!
+    """
+        await e.client.send_file(e.chat_id, DEADLY_IMG, caption=DEADLY_ON, buttons=Deadly_Button)
